1624b2bc30214724a9a4a24d0e1e8c07c402ec71
Raul D <rul.dominguez.r@gmail.com>
2025-07-11 12:20:55 -0300

    más de github actions1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,41 +15,41 @@ require 'rails_helper'
 # spec/models/user_spec.rb
 
 RSpec.describe User, type: :model do
-    describe 'validations' do
-        let(:user) { described_class.new }
+  describe 'validations' do
+    let(:user) { described_class.new }
 
-        it 'validar presencia de datos' do
-            expect(user).to validate_presence_of(:email)
-        end
-
-        it 'validates email format' do
-            expect(build(:user, email: 'invalid')).not_to be_valid
-            expect(build(:user, email: 'valid@example.com')).to be_valid
-        end
-
-        it 'validates password length' do
-            expect(build(:user, password: '12345')).not_to be_valid
-            expect(build(:user, password: '123456')).to be_valid
-        end
+    it 'validar presencia de datos' do
+      expect(user).to validate_presence_of(:email)
     end
 
-    describe 'associations' do
-        it { is_expected.to have_many(:tasks) }
-        it { is_expected.to have_many(:tasks).dependent(:destroy) }
+    it 'validates email format' do
+      expect(build(:user, email: 'invalid')).not_to be_valid
+      expect(build(:user, email: 'valid@example.com')).to be_valid
     end
 
-    describe 'when task mark_as_completed!' do
-        it 'updates completed status' do
-        task = create(:task, completed: false)
-        task.mark_as_completed!
-        expect(task.completed).to be true
-        end
+    it 'validates password length' do
+      expect(build(:user, password: '12345')).not_to be_valid
+      expect(build(:user, password: '123456')).to be_valid
     end
+  end
 
-    describe '#admin?' do
-        it 'returns true for admin users' do
-            admin = build(:user, admin: true)
-            expect(admin.admin?).to be true
-        end
+  describe 'associations' do
+    it { is_expected.to have_many(:tasks) }
+    it { is_expected.to have_many(:tasks).dependent(:destroy) }
+  end
+
+  describe 'when task mark_as_completed!' do
+    it 'updates completed status' do
+      task = create(:task, completed: false)
+      task.mark_as_completed!
+      expect(task.completed).to be true
     end
+  end
+
+  describe '#admin?' do
+    it 'returns true for admin users' do
+      admin = build(:user, admin: true)
+      expect(admin.admin?).to be true
+    end
+  end
 end
